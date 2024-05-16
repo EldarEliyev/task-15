9d0ef1a2dc11a75b673edea0f11f6abad93821da
eldar23451 <liyev8024@gmail.com>
2024-05-16 21:36:07 +0400

    foudantion

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//int a = int.Parse(Console.ReadLine());
+//int b =int.Parse(Console.ReadLine());
+//if (a > b)
+//{
+//    Console.WriteLine("reqem boyukdur");
+
+//}
+//else if (a < b)
+//{
+//    Console.WriteLine("reqem kicikdir");
+//}
+
+using System.ComponentModel.Design;
+
+
+
